63c6bbb7c40a8dfd199c6165c98a93be7ddff858
DeexantBharti <deexantbharti4321@gmail.com>
2025-06-03 14:11:36 +0530

    admin login api

--- api/server.js
+++ api/server.js
@@ -2,8 +2,11 @@ import express from 'express'
 import 'dotenv/config'
 
 import cors from 'cors'
+import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
 
 const app = express();
+await connectDB();
 
 // middleware
 app.use(cors());
@@ -13,6 +16,7 @@ app.use(express.json());
 
 // routes
 app.get('/',(req,res) => res.send("Api is working")) 
+app.use('/api/admin',adminRouter);
 
 const PORT = process.env.PORT || 3000;
 
