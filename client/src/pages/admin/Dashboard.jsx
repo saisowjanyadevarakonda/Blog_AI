2951494a7234efc0f65afd8bd39ccdcccb16c651
Deexant Bharti <deexantbharti4321@gmail.com>
2025-07-06 12:52:54 +0530

    reframing some admin functionality for view mode only

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -3,8 +3,10 @@ import { assets, dashboard_data } from '../../assets/assets'
 import BlogTableItem from '../../components/admin/BlogTableItem';
 import { useAppContext } from '../../context/AppContext';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+  const navigate = useNavigate();
     const [dashboardData,setDashboardData] = useState({
         blogs:0,
         comments:0,
@@ -30,7 +32,7 @@ const Dashboard = () => {
   return (
     <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
         <div className='flex flex-wrap gap-4'>
-            <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+            <div onClick={()=>navigate('/admin/listBlog')} className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
                 <img src={assets.dashboard_icon_1} alt="" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.blogs} </p>
@@ -38,7 +40,7 @@ const Dashboard = () => {
                 </div>
             </div>
 
-     <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+     <div onClick={()=> navigate('/admin/comments')} className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
                 <img src={assets.dashboard_icon_2} alt="" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.comments} </p>
@@ -47,7 +49,7 @@ const Dashboard = () => {
             </div>
 
 
-<div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
+<div onClick={()=> navigate('/admin/listBlog')} className='flex items-center gap-4 bg-white p-4 min-w-58 rounded shadow cursor-pointer hover:scale-105 transition-all'>
                 <img src={assets.dashboard_icon_3} alt="" />
                 <div>
                     <p className='text-xl font-semibold text-gray-600'>{dashboardData.drafts} </p>
